5e8090143e2adff8749d94e0ba225c25590622b0
Victor Mesquitta <mvictormesquita@gmail.com>
2023-11-11 17:45:58 -0300

    crud atualizado com dto do UsuarioController

--- src/main/java/senac/domain/controllers/UsuarioController.java
+++ src/main/java/senac/domain/controllers/UsuarioController.java
@@ -12,6 +12,7 @@ import senac.domain.models.UsuarioModel;
 import senac.domain.repositories.UsuarioRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin("*")
@@ -25,18 +26,8 @@ public class UsuarioController {
         this.usuarioRepository = usuarioRepository;
     }
 
-    @GetMapping
-    public List<UsuarioModel> getAllUsers(){
-        return usuarioRepository.findAll();
-    }
 
-    @GetMapping("/{id}")
-    public UsuarioModel findUserByID(@PathVariable Integer id){
-        return usuarioRepository.findById(id).orElseThrow(() ->
-                new ResponseStatusException(HttpStatus.NOT_FOUND,"Cliente não encontrado"));
-    }
-
-//    @PostMapping()
+    //    @PostMapping()
 //    @ResponseStatus(HttpStatus.CREATED)
 //    public UsuarioModel save(@RequestBody UsuarioModel usuarioModel){
 //        return usuarioRepository.save(usuarioModel);
@@ -49,23 +40,80 @@ public class UsuarioController {
         return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(usuarioModel));
     }
 
-    @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete( @PathVariable Integer id ) {
-        usuarioRepository.findById(id).map(usuarioModel -> {
-            usuarioRepository.delete(usuarioModel);
-            return usuarioModel;
-        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Usuario não encontrado"));
+
+//    @GetMapping
+//    public List<UsuarioModel> getAllUsuarios(){
+//        return usuarioRepository.findAll();
+//    }
+
+    @GetMapping()
+    public ResponseEntity<List<UsuarioModel>> getAllUsuarios(){
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.findAll());
     }
 
-    @PutMapping()
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void update( @PathVariable Integer id,@RequestBody UsuarioModel usuarioModel){
-        usuarioRepository.findById(id).map(existingClient -> {
-            usuarioModel.setCodusuario(existingClient.getCodusuario());
-            usuarioRepository.save(usuarioModel);
-            return existingClient;}).orElseThrow(() ->
-                new ResponseStatusException(HttpStatus.NOT_FOUND,"Usuario não encontrado") );
+
+//    @GetMapping("/{id}")
+//    public UsuarioModel findUserByID(@PathVariable() Integer id){
+//        return usuarioRepository.findById(id).orElseThrow(() ->
+//                new ResponseStatusException(HttpStatus.NOT_FOUND,"Cliente não encontrado"));
+//    }
+
+//    @GetMapping("/{id}")
+//    public UsuarioModel findUserByID(@PathVariable(value = "id") Integer id){
+//        return usuarioRepository.findById(id).orElseThrow(() ->
+//                new ResponseStatusException(HttpStatus.NOT_FOUND,"Usuário não encontrado"));
+//    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneUsuario(@PathVariable(value="id") Integer id){
+        Optional<UsuarioModel> usuarioO = usuarioRepository.findById(id);
+        if(usuarioO.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioO.get());
     }
+
+//    @PutMapping()
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
+//    public void update( @PathVariable Integer id,@RequestBody UsuarioModel usuarioModel){
+//        usuarioRepository.findById(id).map(existingClient -> {
+//            usuarioModel.setCodusuario(existingClient.getCodusuario());
+//            usuarioRepository.save(usuarioModel);
+//            return existingClient;}).orElseThrow(() ->
+//                new ResponseStatusException(HttpStatus.NOT_FOUND,"Usuário não encontrado") );
+
+    @PutMapping("{id}")
+    public ResponseEntity<Object> updateUsuario(@PathVariable(value="id") Integer id, @RequestBody @Valid UsuarioRecordDto usuarioRecordDto) {
+        Optional<UsuarioModel> usuarioO = usuarioRepository.findById(id);
+        if(usuarioO.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado.");
+        }
+        var usuarioModel = usuarioO.get();
+        BeanUtils.copyProperties(usuarioRecordDto, usuarioModel);
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.save(usuarioModel));
+    }
+
+
+
+//    @DeleteMapping("/{id}")
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
+//    public void delete( @PathVariable Integer id ) {
+//        usuarioRepository.findById(id).map(usuarioModel -> {
+//            usuarioRepository.delete(usuarioModel);
+//            return usuarioModel;
+//        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Usuário não encontrado"));
+//    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteUsuario(@PathVariable(value="id") Integer id) {
+        Optional<UsuarioModel> usuarioO = usuarioRepository.findById(id);
+        if(usuarioO.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado.");
+        }
+        usuarioRepository.delete(usuarioO.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Usuário deletado com sucesso.");
+    }
+
+
 
 }
