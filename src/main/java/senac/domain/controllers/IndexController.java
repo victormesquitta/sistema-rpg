a746b1a2ead5d9d909daa4b5b7343557954276ae
Victor Mesquitta <mvictormesquita@gmail.com>
2023-11-11 16:04:58 -0300

    controllers de login e index

---
+++ src/main/java/senac/domain/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package senac.domain.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public ModelAndView hello(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+}
