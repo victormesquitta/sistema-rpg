12d140c450db5653ec784e26a0df2868b8953ca4
Victor Mesquitta <mvictormesquita@gmail.com>
2023-11-27 16:35:06 -0300

    arrumando rotas da home

--- src/main/java/senac/domain/controllers/HomeController.java
+++ src/main/java/senac/domain/controllers/HomeController.java
@@ -3,13 +3,16 @@ package senac.domain.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
 @Controller
-public class IndexController {
-    @GetMapping("/index")
-    public ModelAndView hello(){
+public class HomeController {
+
+    @GetMapping("/")
+    public ModelAndView home(){
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("index");
+        modelAndView.setViewName("home");
         return modelAndView;
     }
 
+    
 }
